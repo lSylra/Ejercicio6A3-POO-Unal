9cdfc33a9dfed721e800c677ee877415330db1c0
lSylra <143262189+lSylra@users.noreply.github.com>
2023-09-18 20:43:28 -0500

    Add files via upload

---
+++ Ejercicio6A3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio6a3;
+
+/**
+ *
+ * @author USER
+ */
+public class Ejercicio6A3 {
+
+    public static void main(String[] args) {
+        Principal form = new Principal();
+        form.setVisible(true);
+    }
+}
